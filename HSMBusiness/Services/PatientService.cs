d5a585887bb6c0513921d4ae458af3f50fdf5180
moha327 <m7amad9595@outlook.com>
2026-08-24 15:50:16 +0300

    Add Result Pattern To PatientService

--- HSMBusiness/Services/PatientService.cs
+++ HSMBusiness/Services/PatientService.cs
@@ -1,4 +1,5 @@
 ﻿using HSMBusiness.Mappers;
+using HSMBusiness.Pattern;
 using HSMDataAccess.DTOs;
 using HSMDataAccess.Entities;
 using HSMDataAccess.RepositoryServices;
@@ -12,34 +13,37 @@ namespace HSMBusiness.Services
 {
     public class PatientService
     {
-    
+        ResultPatern resultPattern = new ResultPatern();
         private readonly PatientRepository _patientRepository;
         public enum enMode { Add, Update }
         public enMode _Mode = enMode.Add;
         public PatientService(PatientRepository patientRepository, enMode Mode = enMode.Add)
         {
-            
             _patientRepository = patientRepository;
             _Mode = Mode;
         }
-        public async Task<List<PatientDTO>> GetAll()
+        public async Task<(int,string?,bool,List<PatientDTO>)> GetAll()
         {
             var patients = await _patientRepository.GetAllAsync();
+            var response = await resultPattern.GiveResponse(200);
             if (patients == null)
             {
-                return null;
+                response = await resultPattern.GiveResponse(404);
+                return (response.Status, response.Response, response.IsSuccess, null);
             }
-            return patients.Select(p => new PatientDTO(p.ID, p.MedicalHistory, p.Status, p.PersonID))
-                .ToList();
+            return (response.Status, response.Response, response.IsSuccess, patients.Select(p => new PatientDTO(p.ID, p.MedicalHistory, p.Status, p.PersonID))
+                .ToList());
         }
-        public async Task<PatientDTO> GetByID(string PatientID)
+        public async Task<(int, string?, bool, PatientDTO)> GetByID(string PatientID)
         {
             var patient = await _patientRepository.GetByIDAsync(PatientID);
+            var response = await resultPattern.GiveResponse(200);
             if (patient == null)
             {
-                return new PatientDTO("", "", false, "");
+                response = await resultPattern.GiveResponse(404);
+                return (response.Status, response.Response, response.IsSuccess, new PatientDTO("", "", false, ""));
             }
-            return new PatientDTO(patient.ID, patient.MedicalHistory, patient.Status, patient.PersonID);
+            return (response.Status, response.Response, response.IsSuccess, new PatientDTO(patient.ID, patient.MedicalHistory, patient.Status, patient.PersonID));
         }
         private async Task<bool> _AddNew(PatientDTO patientDTO)
         {
@@ -48,40 +52,56 @@ namespace HSMBusiness.Services
             patientEntity.ID = NewPatient.ID;
             return patientEntity.ID != "";
         }
-        private async Task<bool> _Update(string ID)
+        private async Task<(int, string?, bool)> _Update(string ID)
         {
             Patient? patientEntity = await _patientRepository.GetByIDAsync(ID);
+            var response = await resultPattern.GiveResponse(200);
             if (patientEntity == null)
-                return false;
-            return await _patientRepository.UpdateAsync(patientEntity);
+            {
+                response = await resultPattern.GiveResponse(404);
+                return (response.Status, response.Response, response.IsSuccess);
+            }
+           
+            return (response.Status, response.Response, await _patientRepository.UpdateAsync(patientEntity));
         }
-        public async Task<bool> Save(PatientDTO patientDTO=null,string ID="")
+        public async Task<(int, string?, bool)> Save(PatientDTO patientDTO=null,string ID="")
         {
+            var response = await resultPattern.GiveResponse(200);
             switch (_Mode)
             {
                 case enMode.Add:
                     if (await _AddNew(patientDTO))
                     {
                         _Mode = enMode.Update;
-                        return true;
+                        return (response.Status, response.Response,response.IsSuccess);
                     }
                     else
                     {
-                        return false;
+                        response = await resultPattern.GiveResponse(500);
+                        return (response.Status, response.Response, response.IsSuccess);
                     }
                 case enMode.Update:
                     return await _Update(ID);
             }
-            return false;
+            response = await resultPattern.GiveResponse(500);
+            return (response.Status, response.Response, response.IsSuccess);
         }
-        public async Task<bool> Delete(string PatientID)
+        public async Task<(int, string?, bool)> Delete(string PatientID)
         {
             var patient = await _patientRepository.GetByIDAsync(PatientID);
+            var response = await resultPattern.GiveResponse(200);
             if (patient == null)
             {
-                return false;
+                response = await resultPattern.GiveResponse(404);
+                return (response.Status, response.Response, response.IsSuccess);
             }
-            return await _patientRepository.DeleteAsync(patient);
+            bool IsDeleted = await _patientRepository.DeleteAsync(patient);
+            if (!IsDeleted)
+            {
+                response = await resultPattern.GiveResponse(500);
+                return (response.Status, response.Response, response.IsSuccess);
+            }
+            return (response.Status, response.Response, response.IsSuccess);
         }
     }
 }
