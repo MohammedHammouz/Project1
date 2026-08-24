3aef14a3474c2357f66fca3ad1af92353cafad51
moha327 <m7amad9595@outlook.com>
2026-08-24 15:07:16 +0300

    Add Result Patten

---
+++ HSMBusiness/Pattern/ResultPatern.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using HSMDataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSMBusiness.Pattern
+{
+    public class ResultPatern
+    {
+        public record ResultResponse(
+            int Status,
+            string Response,
+            bool IsSuccess);
+        public async Task< ResultResponse> GiveResponse(int status)
+        {
+            return status switch
+            {
+                200 => new(200, "The request was successful", true),
+                400 => new(400 ,"The client sent an invalid request", false),
+                401 => new(401, "The user is not authenticated", false),
+                403 => new(403, "The user is authenticated but does not have permission", false),
+                404 => new(404, "The requested resource was not found", false),
+                500 => new(500,"An unexpected error occurred on the server", false),
+                _ => new(500, "An unexpected error occurred on the server", false)
+            };
+        }
+    }
+}
--- HSMBusiness/Services/DoctorService.cs
+++ HSMBusiness/Services/DoctorService.cs
@@ -1,4 +1,6 @@
-﻿using HSMBusiness.dto;
+﻿using Azure;
+using HSMBusiness.dto;
+using HSMBusiness.Pattern;
 using HSMDataAccess.DTOs;
 using HSMDataAccess.Entities;
 using HSMDataAccess.RepositoryServices;
@@ -12,27 +14,7 @@ namespace HSMBusiness
 {
     public class DoctorService
     {
-        //public string ID { get; set; } = null!;
-        //public string DepartmentID { get; set; } = null!;
-        
-        //public string Specialization { get; set; } = null!;
-        //public bool Status { get; set; }
-        
-        //public string UserID { get; set; } = null!;
-       
-        
-        //public DoctorDTO doctorDTO
-        //{
-        //    get
-        //    {
-        //        return DoctorMapper.ToDTO(this);
-        //    }
-        //    set
-        //    {
-        //        DoctorMapper.FromDTO(value, this);
-        //    }
-           
-        //}
+        ResultPatern resultPattern = new ResultPatern();
         public enum enMode { Add = 0, Update }
         public enMode mode = enMode.Add;
         private readonly DoctorRepository _doctor;
@@ -43,34 +25,47 @@ namespace HSMBusiness
             this.mode = mode;
         }
         
-        public async Task<DoctorDTO> GetDoctorByID(string DoctorID)
+        public async Task<(string?,bool,DoctorDTO)> GetDoctorByID(string DoctorID)
         {
             Doctor doctor = await _doctor.GetByIDAsync(DoctorID);
+            var response =await resultPattern.GiveResponse(200);
             if (doctor == null)
             {
-                return null;
+                response =await resultPattern.GiveResponse(404);
+                return (response.Response,response.IsSuccess,null);
             }
-            return new DoctorDTO(DoctorID, doctor.DepartmentID, doctor.Specialization, doctor.Status,
-                 doctor.UserID);
+            return (response.Response, response.IsSuccess, new DoctorDTO(DoctorID, doctor.DepartmentID, doctor.Specialization, doctor.Status,
+                 doctor.UserID));
         }
-        public async Task<bool> Delete(string DoctorID)
+        public async Task<(string?,bool)> Delete(string DoctorID)
         {
             Doctor doctor = await _doctor.GetByIDAsync(DoctorID);
+            var response =await resultPattern.GiveResponse(200);
             if (doctor == null)
             {
-                return false;
+                response =await resultPattern.GiveResponse(404);
+                return (response.Response,response.IsSuccess);
             }
-            return await _doctor.DeleteAsync(doctor);
+            bool IsDeleted = await _doctor.DeleteAsync(doctor);
+            if (!IsDeleted)
+            {
+                var error =await resultPattern.GiveResponse(500);
+                return (error.Response, error.IsSuccess);
+            }
+            return (response.Response,response.IsSuccess);
         }
-        public async Task<List<DoctorDTO>> GetAll()
+        public async Task<(string?,bool,List<DoctorDTO>)> GetAll()
         {
             var doctors = await _doctor.GetAllAsync();
+            var response =await resultPattern.GiveResponse(200);
             if (doctors == null)
             {
-                return null;
+                response =await resultPattern.GiveResponse(500);
+                return (response.Response,response.IsSuccess,null);
             }
-            return doctors.Select(d => new DoctorDTO(d.ID,d.DepartmentID,d.Specialization,d.Status,d.UserID))
-        .ToList();
+
+            return (response.Response,response.IsSuccess, doctors.Select(d => new DoctorDTO(d.ID,d.DepartmentID,d.Specialization,d.Status,d.UserID))
+        .ToList());
         }
         private async Task<bool> Add(DoctorDTO doctorDTO)
         {
@@ -79,29 +74,40 @@ namespace HSMBusiness
             doctorEntity.ID = AddNew.ID;
             return doctorEntity.ID != "";
         }
-        private async Task<bool> Update(string ID)
+        private async Task<(string?, bool)> Update(string ID)
         {
             Doctor? doctorEntity = await _doctor.GetByIDAsync(ID);
-            return await _doctor.UpdateAsync(doctorEntity);
+            var response = await resultPattern.GiveResponse(200);
+            if (doctorEntity == null)
+            {
+                response = await resultPattern.GiveResponse(404);
+                return (response.Response,response.IsSuccess);
+            }
+            response =await resultPattern.GiveResponse(200);
+            return (response.Response,await _doctor.UpdateAsync(doctorEntity));
         }
-        public async Task<bool> Save(DoctorDTO doctorDTO=null,string ID="")
+        public async Task<(string?,bool)> Save(DoctorDTO doctorDTO=null,string ID="")
         {
+            var response = await resultPattern.GiveResponse(200);
             switch (mode)
             {
                 case enMode.Add:
+                    
                     if (await Add(doctorDTO))
                     {
                         mode = enMode.Update;
-                        return true;
+                        return (response.Response, response.IsSuccess);
                     }
                     else
                     {
-                        return false;
+                        response =await  resultPattern.GiveResponse(500);
+                        return  (response.Response,response.IsSuccess);
                     }
                 case enMode.Update:
                     return await Update(ID);
             }
-            return false;
+            response = await resultPattern.GiveResponse(500);
+            return (response.Response,response.IsSuccess);
         }
     }
 }
